21c5d127704e5744a0f8d8062ce1bdb2367d8f82
benegative <khannikkey@gmail.com>
2022-09-20 18:34:50 +0100

    design card and align data

--- src/data.jsx
+++ src/data.jsx
@@ -1,21 +1,37 @@
 export default [
     {
-        id: 1,
-        name: 'Nadeem Qureshi',
-        age: 27,
-        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
-    
+      id: 1,
+      name: 'Nadeeem',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
     },
     {
-        id: 2,
-        name: 'Jiten Naikane',
-        age: 37,
-        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
+      id: 2,
+      name: 'Jiten',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
     },
     {
-        id: 1,
-        name: 'ShaktiMaan',
-        age: 47,
-        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
+      id: 3,
+      name: 'prachit',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     },
-]
+    {
+      id: 4,
+      name: 'Niikita',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Shaktiman',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
