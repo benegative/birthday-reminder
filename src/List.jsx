87af1abbbe42f6e7d3184fd42caf231f4584d785
benegative <khannikkey@gmail.com>
2022-09-20 18:14:44 +0100

    data added

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <React.Fragment>
+        <h1>listt</h1>
+    </React.Fragment>
+  )
+}
+
+export default List
---
+++ src/data.jsx
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        name: 'Nadeem Qureshi',
+        age: 27,
+        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
+    
+    },
+    {
+        id: 2,
+        name: 'Jiten Naikane',
+        age: 37,
+        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
+    },
+    {
+        id: 1,
+        name: 'ShaktiMaan',
+        age: 47,
+        image: 'https://cdn.pixabay.com/photo/2020/09/19/19/37/landscape-5585247_960_720.jpg',
+    },
+]
